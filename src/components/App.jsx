e911a9754599c732c557a4bdd1ac30e323793245
Olga <hloesilent@gmail.com>
2020-08-10 23:18:01 +0300

    hohoho

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,7 +37,7 @@ class App extends Component {
             moviesWillWatch: updateMoviesWillWatch
           });
       console.log(updateMoviesWillWatch);
-    }
+    };
 
     render() {
         console.log("render");
